78dff8027c10bbd1ab041e238b116b03d0251dac
AshiqurRahaman02 <ashiqur999999@gamil.com>
2023-07-22 10:10:37 +0530

    adding cors

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+	cors({
+		origin: "*",
+		credentials: true,
+	})
+);
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
